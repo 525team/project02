a795926df09388ceead02c09c5a051d55e7e5e97
Haoyu Miao <945229411@qq.com>
2021-09-23 15:58:34 +0800

    add read_data code

--- src/read_data.py
+++ src/read_data.py
@@ -2,11 +2,43 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-read_training_sample = np.load('../2021_manufacturing_competition_data/training_samples.npy', allow_pickle=True).item()
-# training_sample = read_training_sample[()]
-# read_training_sample.item()
-condition0_ball = read_training_sample['condition0']['ball']
-print(read_training_sample['condition0'])
+# --- training_samples.npy ---
+# read_training_sample = np.load('../2021_manufacturing_competition_data/training_samples.npy', allow_pickle=True).item()
+# condition0_ball = read_training_sample['condition0']['ball']
+# condition0_holder = read_training_sample['condition0']['holder']
+# condition0_inner = read_training_sample['condition0']['inner']
+# condition0_normal = read_training_sample['condition0']['normal']
+# condition0_outer = read_training_sample['condition0']['outer']
+#
+# condition1_ball = read_training_sample['condition1']['ball']
+# condition1_holder = read_training_sample['condition1']['holder']
+# condition1_inner = read_training_sample['condition1']['inner']
+# condition1_normal = read_training_sample['condition1']['normal']
+# condition1_outer = read_training_sample['condition1']['outer']
+# print(read_training_sample['condition0'])
+# plt.plot(condition0_ball)
+# plt.show()
 
-plt.plot(condition0_ball)
-plt.show()
+
+
+# --- final_test.npy ---  200*1024
+# read_final_test = np.load('../2021_manufacturing_competition_data/final_test.npy', allow_pickle=True)
+# names = locals()
+# for i in range(200):
+#     names['final_test_' + str(i)] = read_final_test[i]
+# # print(final_test_0)
+
+
+
+# --- unlabeled_support_samples.npy ---  250*1024
+# read_unlabeled_support_samples = np.load('../2021_manufacturing_competition_data/unlabeled_support_samples.npy', allow_pickle=True)
+
+
+# --- validation_samples.npy ---  9*1024
+# read_validation_samples = np.load('../2021_manufacturing_competition_data/validation_samples.npy', allow_pickle=True).item()
+# validation_samples_ball = read_validation_samples['ball']
+# validation_samples_holder = read_validation_samples['holder']
+# validation_samples_inner = read_validation_samples['inner']
+# validation_samples_normal = read_validation_samples['normal']
+# validation_samples_outer = read_validation_samples['outer']
+
