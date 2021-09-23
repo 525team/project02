79e0ab716411598065d294635195c392c645d9ee
Haoyu Miao <945229411@qq.com>
2021-09-23 14:37:24 +0800

    add read_data code

--- src/read_data.py
+++ src/read_data.py
@@ -1,3 +1,12 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-training_samples_data = np.load('./2021_manufactoring_competition_data/training_samples.npy')
+read_training_sample = np.load('../2021_manufacturing_competition_data/training_samples.npy', allow_pickle=True).item()
+# training_sample = read_training_sample[()]
+# read_training_sample.item()
+condition0_ball = read_training_sample['condition0']['ball']
+print(read_training_sample['condition0'])
+
+plt.plot(condition0_ball)
+plt.show()
