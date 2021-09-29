8b6bf4b7706e135b337ecd0c2cb42fc310b1b996
Haoyu Miao <945229411@qq.com>
2021-09-29 17:24:27 +0800

    update predict_utils_classification.py

--- src/predict_advanced.py
+++ src/predict_advanced.py
@@ -2,11 +2,11 @@ from src.utils.predict_utils_classification import predict_utils_classification 
 from train_advanced import parse_args
 
 # test ------ predictor
-PTH = '.\checkpoint\DA\s_0_1_t2\cnn_features_1d_0927-190704\8-0.3333-best_model.pth'
+PTH = '.\checkpoint\DA\s_0_1_t2\cnn_features_1d_0928-234342\99-0.2353-best_model.pth'
 fault_name = ['inner', 'outer', 'ball', 'holder', 'normal']
 len_val_sample = 9
 args = parse_args()
 
 predictor = predictor(args, PTH)
 predictor.setup()
-correct_rate_overall, correct_rate = predictor.predict(fault_name, len_val_sample)
+correct_rate_overall, correct_rate = predictor.predict(len(fault_name), len_val_sample)
--- src/utils/predict_utils_classification.py
+++ src/utils/predict_utils_classification.py
@@ -1,7 +1,11 @@
+import logging
+import warnings
 import torch
+from torch import nn
 import numpy as np
 import os
 from scipy import io
+from src import datasets
 import src.models as models
 
 # new version of load validation data
@@ -23,9 +27,32 @@ class predict_utils_classification():
     def setup(self):
         args = self.args
         # load model
+
+        # Consider the gpu or cpu condition
+        if torch.cuda.is_available():
+            self.device = torch.device("cuda")
+            self.device_count = torch.cuda.device_count()
+            logging.info('using {} gpus'.format(self.device_count))
+            assert args.batch_size % self.device_count == 0, "batch size should be divided by device count"
+        else:
+            warnings.warn("gpu is not available")
+            self.device = torch.device("cpu")
+            self.device_count = 1
+            logging.info('using {} cpu'.format(self.device_count))
+
+        Dataset = getattr(datasets, args.data_name)
         self.model = getattr(models, args.model_name)(args.pretrained)
-        self.model.load_state_dict(torch.load(self.model_pth))
-        self.model.eval()
+        if args.bottleneck:
+            self.bottleneck_layer = nn.Sequential(nn.Linear(self.model.output_num(), args.bottleneck_num),
+                                                  nn.ReLU(inplace=True), nn.Dropout())
+            self.classifier_layer = nn.Linear(args.bottleneck_num, Dataset.num_classes)
+        else:
+            self.classifier_layer = nn.Linear(self.model.output_num(), Dataset.num_classes)
+
+        self.model_all = nn.Sequential(self.model, self.bottleneck_layer, self.classifier_layer)
+        self.model_all.load_state_dict(torch.load(self.model_pth))
+        self.model_all.eval()
+
 
     def predict(self, fault_num, val_sample_num_per_fault):
         # predict data
@@ -34,8 +61,9 @@ class predict_utils_classification():
             for i in range(fault_num):
                 correct_i = 0
                 for j in range(val_sample_num_per_fault):
-                    # 下面这行代码我不确定
-                    label_output = self.model(read_validation_samples[fault_name[i]][j])
+                    inputs = torch.from_numpy(read_validation_samples[fault_name[i]][j])
+                    inputs = inputs.to(self.device)
+                    label_output = self.model_all(inputs)
                     if label_output == i:
                         correct_i += 1
                 correct.append(correct_i)
